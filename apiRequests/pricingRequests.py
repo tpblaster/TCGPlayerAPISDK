79d8bc7778e4606abfb7c4f331584f07aa638110
tpblaster <promalcolm@gmail.com>
2020-07-17 22:34:46 -0400

    changed error handling so when a 401 is returned the invalid bearer token error is raised

--- apiRequests/pricingRequests.py
+++ apiRequests/pricingRequests.py
@@ -1,12 +1,14 @@
 import json
-
+from errors import invalid_bearer_token
 import requests
 
 
 # This endpoint takes a product condition id or a list of them and returns market data for them
 # if you want pricing data for a single id then then use single_sku_market_price
 # you can pass up to 250 sku ids into this endpoint
-def list_sku_market_price(bearer, sku_list: list):
+
+
+def list_sku_market_price(bearer: str, sku_list: list):
     if len(sku_list) > 250:
         return None
     url = "https://api.tcgplayer.com/pricing/sku/" + ','.join(sku_list)
@@ -14,5 +16,7 @@ def list_sku_market_price(bearer, sku_list: list):
     response = requests.request("GET", url, headers=headers)
     if response.status_code == 200:
         return json.loads(response.text)["results"]
+    elif response.status_code == 401:
+        raise invalid_bearer_token()
     else:
         return None

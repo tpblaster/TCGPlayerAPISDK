80948e22097e0478cb42ef76b845134e31506b86
tpblaster <promalcolm@gmail.com>
2020-07-23 15:19:55 -0400

    Added a function to parse JSON data from the TCG API into an object, not sure if it will get used

--- cardClasses/pricingDataClasses.py
+++ cardClasses/pricingDataClasses.py
@@ -10,3 +10,9 @@ class SkuPricingData:
     def create_from_dict(request_result: dict):
         price_object = SkuPricingData(request_result["skuId"], request_result["lowPrice"], request_result["lowestShipping"], request_result["lowestListingPrice"], request_result["marketPrice"], request_result["directLowPrice"])
         return price_object
+
+    def create_from_list_dict(result_list: list):
+        list_sku_pricing = []
+        for i in result_list:
+            list_sku_pricing.append(SkuPricingData.create_from_dict(i))
+        return list_sku_pricing

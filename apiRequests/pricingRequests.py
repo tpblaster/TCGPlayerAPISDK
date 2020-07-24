0ae7c2be678b0a6ef70b2c9b262bcddae2d2e3ab
tpblaster <promalcolm@gmail.com>
2020-07-24 16:24:20 -0400

    Added several new endpoints, added handling for 401 bad bearer requests

--- apiRequests/pricingRequests.py
+++ apiRequests/pricingRequests.py
@@ -1,6 +1,7 @@
 import json
+
 import requests
-from cardClasses.pricingDataClasses import SkuPricingData
+
 from errors import InvalidBearerToken, InvalidListLength, InvalidPricingRequest, InvalidGroupId, NoDataFoundForGroup
 
 
@@ -66,6 +67,8 @@ def get_all_price_group(bearer: str, group_id: int):
     # Request had an invalid group id and is either 0 or less than 0
     elif response.status_code == 400:
         raise InvalidGroupId()
+    elif response.status_code == 401:
+        raise InvalidBearerToken()
     # Request returned no data or group is above 0 but invalid
     elif response.status_code == 404:
         raise NoDataFoundForGroup()
@@ -89,6 +92,8 @@ def get_product_list_market_price(bearer: str, product_list: list):
     # Returns all data found but some product ids had no data, those with no data are indicated in the error section
     elif response.status_code == 207:
         return json.loads(response.text)
+    elif response.status_code == 401:
+        raise InvalidBearerToken()
     # All ids were invalid or no data was found
     elif response.status_code == 404:
         raise InvalidPricingRequest()
@@ -103,6 +108,100 @@ def get_single_product_market_price(bearer: str, product_id: int):
     # Returns a list of pricing data associated with the given product id
     if response.status_code == 200:
         return json.loads(response.text)["results"]
+    elif response.status_code == 401:
+        raise InvalidBearerToken()
     # No data was found or the product id was invalid
+    elif response.status_code == 404:
+        raise InvalidPricingRequest()
+
+
+# Takes a list of product ids and returns buylist data
+# Takes a list of integer based product ids
+def get_product_list_buylist_price(bearer: str, product_list: list):
+    # Handles potential 400 responses
+    if len(product_list) > 250 or len(product_list) < 2:
+        raise InvalidListLength(len(product_list))
+    url = "https://api.tcgplayer.com/pricing/buy/product/" + ','.join(map(str, product_list))
+    headers = {'Authorization': 'Bearer ' + bearer}
+    response = requests.request("GET", url, headers=headers)
+    # All product ids had data and it is all returned
+    if response.status_code == 200:
+        return json.loads(response.text)
+    # Some product ids had data, those that didn't are returned in the error section
+    elif response.status_code == 207:
+        return json.loads(response.text)
+    elif response.status_code == 401:
+        raise InvalidBearerToken()
+    # No data was found or all product ids were invalid
+    elif response.status_code == 404:
+        raise InvalidPricingRequest()
+
+
+# Takes a single product id and returns all buylist data for skus related to the id
+
+def get_single_product_buylist_price(bearer: str, product_id: int):
+    url = "https://api.tcgplayer.com/pricing/buy/product/{}".format(product_id)
+    headers = {'Authorization': 'Bearer ' + bearer}
+    response = requests.request("GET", url, headers=headers)
+    # Request was successful
+    if response.status_code == 200:
+        return json.loads(response.text)["results"]
+    elif response.status_code == 401:
+        raise InvalidBearerToken()
+    elif response.status_code == 404:
+        raise InvalidPricingRequest()
+
+
+# Takes an int based list of sku ids and returns buylist data for them
+
+def get_sku_list_buylist_price(bearer: str, sku_list: str):
+    # Handles potential 400 responses
+    if len(sku_list) > 250 or len(sku_list) < 2:
+        raise InvalidListLength(len(sku_list))
+    url = "https://api.tcgplayer.com/pricing/buy/sku/" + ','.join(map(str, sku_list))
+    headers = {'Authorization': 'Bearer ' + bearer}
+    response = requests.request("GET", url, headers=headers)
+    # Request was successful, every sku is returned with data
+    if response.status_code == 200:
+        return json.loads(response.text)
+    # Some sku ids did not have data and are returned in the error section
+    elif response.status_code == 207:
+        return json.loads(response.text)
+    elif response.status_code == 401:
+        raise InvalidBearerToken()
+    # No data was found for all sku ids or all sku ids were invalid
+    elif response.status_code == 404:
+        raise InvalidPricingRequest()
+
+
+# Takes a single sku id and returns pricing data
+
+def get_single_sku_buylist_price(bearer: str, sku_id: int):
+    url = "https://api.tcgplayer.com/pricing/buy/sku/{}".format(sku_id)
+    headers = {'Authorization': 'Bearer ' + bearer}
+    response = requests.request("GET", url, headers=headers)
+    if response.status_code == 200:
+        return json.loads(response.text)["results"]
+    elif response.status_code == 401:
+        raise InvalidBearerToken()
+    # No data was found for the sku id or the sku id was invalid
+    elif response.status_code == 404:
+        raise InvalidPricingRequest()
+
+
+# Gets buylist prices for a set based on the group id
+
+def get_group_buylist_price(bearer: str, group_id: int):
+    url = "https://api.tcgplayer.com/pricing/buy/group/{}".format(group_id)
+    headers = {'Authorization': 'Bearer ' + bearer}
+    response = requests.request("GET", url, headers=headers)
+    if response.status_code == 200:
+        return json.loads(response.text)["results"]
+    # Request had an invalid group id and is either 0 or less than 0
+    elif response.status_code == 400:
+        raise InvalidGroupId()
+    elif response.status_code == 401:
+        raise InvalidBearerToken()
+    # No data was found for the group id or it was invalid
     elif response.status_code == 404:
         raise InvalidPricingRequest()
